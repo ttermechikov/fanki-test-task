cf743cd6b64d3627555d12f026a39d7e30a2034d
Temirlan <temirlan.termechikov@gmail.com>
2020-12-14 00:45:43 +0600

    Edited the main page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,97 @@
+import { Component } from 'react';
 import Header from './components/Header/Header.js';
-import './App.css';
+import Menu from './components/Menu/Menu.js';
+import ControlBar from './components/ControlBar/ControlBar.js';
+import EmployesTable from './components/EmployesTable/EmployesTable.js';
 
-function App() {
-  return <Header />;
+import './App.scss';
+
+class App extends Component {
+  #employe = {
+    name: 'Александров Александр Александрович',
+    work: {
+      position: 'Frontend Разработчик',
+      department: 'Frontend Department',
+    },
+    tel: '+996 770 000 000',
+    mail: 'alexandrov.alexandr@fankihr.com',
+    passport: {
+      id: '00 00 123456',
+      district: 'УФМС России По Московской области',
+      date: '01.01.2012 000-000',
+    },
+    education: {
+      degree: 'Высшее',
+      specialty: 'Разработчик программно-информационных систем',
+    },
+    isWorking: true,
+  };
+
+  constructor() {
+    super();
+    this.state = {
+      companyMenu: {
+        title: 'Company name',
+        items: [
+          { id: 1, item: 'Главная', icon: 'main-icon.svg' },
+          { id: 2, item: 'Запросы', icon: 'flash-icon.svg' },
+          { id: 3, item: 'Календарь', icon: 'calendar-icon.svg' },
+          { id: 4, item: 'База знаний', icon: 'book-icon.svg' },
+          { id: 5, item: 'Новости', icon: 'book-open-icon.svg' },
+          { id: 6, item: 'Задачи', icon: 'checkmark-circle-icon.svg' },
+          { id: 7, item: 'Чаты', icon: 'message-circle-icon.svg' },
+        ],
+      },
+      adminMenu: {
+        title: 'Администратор',
+        items: [
+          { id: 8, item: 'Статистика компании', icon: 'trending-up-icon.svg' },
+          { id: 9, item: 'Структура компании', icon: 'copy-icon.svg' },
+          { id: 10, item: 'Element text', icon: 'folder-icon.svg' },
+          { id: 11, item: 'Сотрудники', icon: 'people-icon.svg' },
+          { id: 12, item: 'База знаний', icon: 'book-icon.svg' },
+          { id: 13, item: 'Новости', icon: 'book-open-icon.svg' },
+          { id: 14, item: 'Настройки', icon: 'settings-icon.svg' },
+          { id: 15, item: 'Служба поддержки', icon: 'layers-icon.svg' },
+        ],
+      },
+      selectedMenuItem: 11,
+      employes: Array(15).fill(this.#employe),
+    };
+  }
+
+  onMenuClick = (e) => {
+    const selectedMenuItem = e.target.getAttribute('dataid');
+    this.setState({ selectedMenuItem });
+  };
+
+  render() {
+    const { companyMenu, adminMenu, selectedMenuItem, employes } = this.state;
+
+    return (
+      <>
+        <Header />
+        <main className="main">
+          <aside className="aside">
+            <Menu
+              selectedMenuItem={selectedMenuItem}
+              onMenuClick={this.onMenuClick}
+              {...companyMenu}
+            />
+            <Menu
+              selectedMenuItem={selectedMenuItem}
+              onMenuClick={this.onMenuClick}
+              {...adminMenu}
+            />
+          </aside>
+          <div className="content">
+            <ControlBar />
+            <EmployesTable employes={employes} />
+          </div>
+        </main>
+      </>
+    );
+  }
 }
 
 export default App;
